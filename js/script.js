b43a33a38f746e5542e722eb377b608edf402ca0
Donald Neuterman <dneuterman@gmail.com>
2022-01-07 02:54:05 -0500

    added seetings buttons. refactored js and added settings functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
-const SQUARE_GRID_COUNT = 64;
+let squareGridCount = 16;
 const GRID_SIZE = 960;
-const boxSize = GRID_SIZE / SQUARE_GRID_COUNT;
+let boxSize = GRID_SIZE / squareGridCount;
 const mainContainer = document.querySelector('.main-container');
+const resetButton = document.querySelector('.reset');
+const blackSetting = document.querySelector('.black-setting');
+const rainbowSetting = document.querySelector('.rainbow-setting');
+const colorPicker = document.querySelector('#color-picker');
+const gridResize = document.querySelector('#grid-resize');
+const gridResizeLabel = document.querySelector('label[for="grid-resize"]');
+
+const randomColorValue = () => {
+  return Math.floor(Math.random() * 256)
+}
+
+const boxMouseOverColor = (rainbow = false, color = '#000000') => {
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => {
+    box.addEventListener('mouseover', () => {
+      if (rainbow) {
+        box.style.backgroundColor = `rgb(${randomColorValue()}, ${randomColorValue()}, ${randomColorValue()})`;
+      } else {
+        box.style.backgroundColor = color;
+      }
+    })
+  })
+}
 
 const createGrid = () => {
-  for (i = 0; i < SQUARE_GRID_COUNT; i++) {
+  if (document.querySelectorAll('.box-row')) {
+    const oldBoxRows = document.querySelectorAll('.box-row');
+    oldBoxRows.forEach(oldBoxRow => {
+      oldBoxRow.remove();
+    })
+  }
+  for (i = 0; i < squareGridCount; i++) {
     const boxRow = document.createElement('div');
     boxRow.classList.add('box-row');
-    for(j = 0; j < SQUARE_GRID_COUNT; j++) {
+    for (j = 0; j < squareGridCount; j++) {
       const box = document.createElement('div');
       box.classList.add('box');
       box.setAttribute('style', `height: ${boxSize}px; width: ${boxSize}px`)
@@ -15,13 +44,33 @@ const createGrid = () => {
     }
     mainContainer.append(boxRow);
   }
-  
+}
+
+const resetGrid = () => {
   const boxes = document.querySelectorAll('.box');
   boxes.forEach(box => {
-    box.addEventListener('mouseover', (event) => {
-      event.target.style.backgroundColor = "red";
-    })
+    box.style.backgroundColor = "#ffffff";
   })
 }
 
-createGrid()
+resetButton.addEventListener('click', resetGrid);
+blackSetting.addEventListener('click', () => {
+  boxMouseOverColor();
+  colorPicker.value = "#000000";
+});
+rainbowSetting.addEventListener('click', () => boxMouseOverColor(true));
+colorPicker.addEventListener('change', (event) => boxMouseOverColor(false, event.target.value));
+gridResize.addEventListener('input', (event) => {
+  gridResizeLabel.textContent = `${event.target.value} x ${event.target.value}`
+});
+gridResize.addEventListener('change', (event) => {
+  console.log(event.target.value)
+  squareGridCount = event.target.value;
+  boxSize = GRID_SIZE / squareGridCount
+  console.log(boxSize)
+  createGrid();
+  boxMouseOverColor(false, colorPicker.value);
+});
+
+createGrid();
+boxMouseOverColor();
